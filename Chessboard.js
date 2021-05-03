de02ca0b1e5ad2af558c2bb5764e9f840a64d776
Gajol <doug@nielsenfamily.ca>
2021-05-03 12:27:45 -0400

    Update Chessboard.js

--- Chessboard.js
+++ Chessboard.js
@@ -7,9 +7,8 @@ for (i=0; i < boardsize; i++)
     // columns
     for (j=0; j < boardsize; j++)
     {
-      j % 2 ? boardstring += startchar : boardstring += nextchar
+      (i + j) % 2 ? boardstring += startchar : boardstring += nextchar
     }
     boardstring += "\n"
-    i % 2 ? (startchar = " ", nextchar = "#") : (startchar = "#", nextchar = " ")
 }
 console.log (boardstring)
